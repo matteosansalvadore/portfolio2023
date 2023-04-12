af84b7744b15e4a3c900225d3d03d1921db935b6
Matteo Sansalvadore <matteosansalvadore@gmail.com>
2023-04-12 16:58:19 +0200

    added Skills

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,13 +14,17 @@ const Container = styled.div`
 
 const Introduction = styled.div`
   height: 300px;
-  background-color: black;
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url("../forest.jpg");
+  background-position: center;
+  background-size: cover;
   border-radius: 10px;
   justify-content: center;
   display: flex;
   padding-left: 30px;
   flex-direction: column;
   width: 100%;
+  box-shadow: 0px 2px 5px 1px rgba(199, 201, 204, 0.76);
 `;
 
 const Hey = styled.h1`
@@ -41,21 +45,80 @@ const ProfileCard = styled.div`
   height: 300px;
   background-color: #ffffff;
   border-radius: 10px;
-  box-shadow: 0px 2px 5px 1px rgba(199, 201, 204, 0.76);
+  box-shadow: 0px 3px 4px 1px rgba(199, 201, 204, 0.76);
   align-items: center;
   justify-content: center;
   display: flex;
   flex-direction: column;
+  flex-shrink: 0;
 `;
 
 const Img = styled.img`
   width: 130px;
   height: 130px;
   border-radius: 50%;
+  box-shadow: 0px 4px 4px 1px rgba(199, 201, 204, 0.76);
 `;
 
 const Name = styled.h3`
   font-size: 18px;
+  margin: 20px 0 5px 0;
+  text-shadow: 0px 2px 5px 1px rgba(199, 201, 204, 0.76);
+`;
+
+const JobTitle = styled.p`
+  font-size: 14px;
+  color: #6b7280;
+  margin: 0;
+  text-shadow: 0px 2px 5px 1px rgba(199, 201, 204, 0.76);
+`;
+
+const Location = styled.div`
+  background-color: #d1fae5;
+  padding: 4px 10px;
+  color: #065f46;
+  border-radius: 12px;
+  font-size: 14px;
+  width: fit-content;
+  margin: 10px 0;
+  box-shadow: 0px 3px 3px 1px rgba(199, 201, 204, 0.76);
+`;
+
+const SkillsComponent = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  gap: 20px;
+`;
+
+const SkillsBoxes = styled.div`
+  flex-grow: 1;
+  flex-basis: 0;
+  flex-shrink: 0;
+  height: 90px;
+  border-radius: 10px;
+  box-shadow: 0px 1px 2px 1px rgba(199, 201, 204, 0.76);
+  background-color: #ffffff;
+  align-items: center;
+  padding-left: 20px;
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const Skills = styled.p`
+  font-size: 18px;
+  color: #111827;
+`;
+
+const SkillsImg = styled.img`
+  width: 50px;
+`;
+
+const SkillsText = styled.p`
+  font-size: 16px;
+  color: #111827;
+  margin-left: 20px;
 `;
 
 export default function Home() {
@@ -77,8 +140,39 @@ export default function Home() {
         <ProfileCard>
           <Img src="../FotoMatteo.jpeg"></Img>
           <Name>Matteo Sansalvadore</Name>
+          <JobTitle>Frontend Developer</JobTitle>
+          <Location>Turin, Italy</Location>
         </ProfileCard>
       </Container>
+      <Skills>Skills</Skills>
+      <SkillsComponent>
+        <SkillsBoxes>
+          <SkillsImg src="../Javascript-logo.png"></SkillsImg>
+          <SkillsText>JavaScript (ES6+)</SkillsText>
+        </SkillsBoxes>
+        <SkillsBoxes>
+          <SkillsImg src="../React-icon.png"></SkillsImg>
+          <SkillsText>React</SkillsText>
+        </SkillsBoxes>
+        <SkillsBoxes>
+          <SkillsImg src="../nextjs-icon.svg"></SkillsImg>
+          <SkillsText>NextJs</SkillsText>
+        </SkillsBoxes>
+      </SkillsComponent>
+      <SkillsComponent>
+        <SkillsBoxes>
+          <SkillsImg src="../css.png"></SkillsImg>
+          <SkillsText>CSS</SkillsText>
+        </SkillsBoxes>
+        <SkillsBoxes>
+          <SkillsImg src="../sc.png"></SkillsImg>
+          <SkillsText>Styled Components</SkillsText>
+        </SkillsBoxes>
+        <SkillsBoxes>
+          <SkillsImg src="../Ruby_On_Rails_Logo.png"></SkillsImg>
+          <SkillsText>Ruby on Rails</SkillsText>
+        </SkillsBoxes>
+      </SkillsComponent>
     </>
   );
 }
